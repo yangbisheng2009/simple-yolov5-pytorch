f04ec3b8c246d0147b69127bf9d8f3bd6281ee6d
Biuke Yang (杨风光) <yangfg@inspur.com>
2020-06-21 16:12:51 +0800

    modify detect

---
+++ detect1.py
@@ -0,0 +1,113 @@
+import argparse
+
+from utils.datasets import *
+from utils.utils import *
+from models.yolo import Model
+
+
+def detect(save_img=True):
+    out, source, weights, view_img, save_txt, imgsz = \
+        opt.output, opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
+
+    # Initialize
+    device = torch_utils.select_device(opt.device)
+    if os.path.exists(opt.output_images):
+        shutil.rmtree(opt.output_images)  # delete output folder
+    os.makedirs(opt.output_images)  # make new output folder
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    google_utils.attempt_download(weights)
+    #model = torch.load(weights, map_location=device)['model'].float()  # load to FP32
+    model = Model(opt.cfg).to(device)
+    model.load_state_dict(torch.load(weights))
+    model.to(device).eval()
+    if half:
+        model.half()  # to FP16
+
+    # Set Dataloader
+    dataset = LoadImages(source, img_size=imgsz)
+
+    # Get names and colors
+    names = model.names if hasattr(model, 'names') else model.modules.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
+
+    # Run inference
+    t0 = time.time()
+    img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
+    _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
+    for f in os.listdir(opt.source):
+        path = os.path.join(opt.source, f)
+        img0 = cv2.imread(path)
+        img_ = letterbox(img0, new_shape=opt.img_size)[0]
+        img_ = img_[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+        img_ = np.ascontiguousarray(img_)
+
+        img = torch.from_numpy(img_).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Inference
+        t1 = torch_utils.time_synchronized()
+        pred = model(img, augment=opt.augment)[0]
+
+        # Apply NMS
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres,
+                                   fast=True, classes=opt.classes, agnostic=opt.agnostic_nms)
+        t2 = torch_utils.time_synchronized()
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            p, s, im0 = path, '', img0
+
+            s += '%gx%g ' % img.shape[2:]  # print string
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  #  normalization gain whwh
+            if det is not None and len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += '%g %ss, ' % (n, names[int(c)])  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in det:
+                    if save_img or view_img:  # Add bbox to image
+                        label = '%s %.2f' % (names[int(cls)], conf)
+                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+
+            # Print time (inference + NMS)
+            print('%sDone. (%.3fs)' % (s, t2 - t1))
+
+            # Save results (image with detections)
+            cv2.imwrite(opt.output_images, im0)
+
+    print('Done. (%.3fs)' % (time.time() - t0))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', type=str, default='weights/yolov5s.pt', help='model.pt path')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--output', type=str, default='inference/output', help='output folder')  # output folder
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.4, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
+    parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--cfg', default='confgs/coco.yaml', help='cfg')
+    parser.add_argument('--output-images', default='output-images', help='output images')
+    opt = parser.parse_args()
+    opt.img_size = check_img_size(opt.img_size)
+    print(opt)
+
+    with torch.no_grad():
+        detect()
