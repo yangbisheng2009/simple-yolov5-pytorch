95ae2dff3bcc749b36684fd761acf72c0b432037
yangbisheng2009 <yangbisheng2009@gmail.com>
2020-11-08 16:00:35 +0800

    add smo configs

--- run_predict.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python predict.py --checkpoint checkpoints/silkworm-m/111_0.9712_0.7970_0.8145.pth -p configs/silkworm/silkworm-m.yaml --input-images /home/yangfg/work/corpus/silkworm/voc/JPEGImages/ --output-images outputs/ --agnostic-nms
