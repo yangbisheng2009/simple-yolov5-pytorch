2f3f2043324125af40a8acf24dac29a55489aa05
yangbisheng2009 <yangbisheng2009@gmail.com>
2020-12-13 15:12:46 +0800

    car count

---
+++ scripts/take_pic.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$1" = "train" ];
+then
+    CUDA_VISIBLE_DEVICES=0 nohup python train.py --project configs/take-pic/take-pic-m.yaml 1>log.log 2>err.log &
+elif [ "$1" = "predict" ];
+then
+    python 1_predict_image.py --project configs/helmet-reflect/helmet-reflect.yaml --input-images /home/work/datasets/helmet-reflect/yolo/images/val/ --output-images outputs --agnostic-nms --checkpoint checkpoints/helmet-reflect-m/85_0.8936_0.6007_0.6300.pth
+fi
