67f860107ee373d90f8067a2b197237d73e8a566
yangbisheng2009 <yangbisheng2009@gmail.com>
2020-12-13 18:52:08 +0800

    car count

--- scripts/car_count.sh
+++ scripts/car_count.sh
@@ -5,5 +5,5 @@ then
     CUDA_VISIBLE_DEVICES=0 nohup python train.py --project configs/car-count/car-count-m.yaml 1>log.log 2>err.log &
 elif [ "$1" = "predict" ];
 then
-    python 1_predict_image.py --project configs/helmet-reflect/helmet-reflect.yaml --input-images /home/work/datasets/helmet-reflect/yolo/images/val/ --output-images outputs --agnostic-nms --checkpoint checkpoints/helmet-reflect-m/85_0.8936_0.6007_0.6300.pth
+    python 1_predict_image.py --project configs/car-count/car-count-m.yaml --input-images /home/work/datasets/car-count/yolo/images/val/ --output-images outputs --agnostic-nms --checkpoint checkpoints/car-count-m/54_0.9694_0.6940_0.7215.pth
 fi
